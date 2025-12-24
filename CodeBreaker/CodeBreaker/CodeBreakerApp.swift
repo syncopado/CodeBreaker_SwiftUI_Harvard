75369f2408d487c942ab7cf209bb53a369be0fe8
Jhernie Magnampo <magnampo_jhernie28@yahoo.com>
2025-12-23 21:37:35 -0500

    new project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Jhernie Magnampo on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Jhernie Magnampo on 2025-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Tanga!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
